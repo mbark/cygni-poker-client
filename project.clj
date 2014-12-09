a4287ec8e4661035bcb506b4f3da7115e796e2a2
mbark <barksten@kth.se>
2014-12-09 19:06:20 +0100

    Log to file instead of terminal

--- project.clj
+++ project.clj
@@ -4,8 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main poker-client.simple-bot
+  :aot [poker-client.simple-bot]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.2.4"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [camel-snake-kebab "0.1.5"]])
+                 [camel-snake-kebab "0.1.5"]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]])
