d4fda3c64b350f5614b92a9cda2fa48448885dab
mbark <barksten@kth.se>
2015-10-14 22:54:10 +0200

    Bump version in project file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject poker-client "0.1.0-SNAPSHOT"
+(defproject poker-client "1.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
